419345d85113bde52db92463be0a4e237bde17be
Rita <rkaufm04@campus.haifa.ac.il>
2016-03-22 14:01:38 +0200

    added utils - initial

--- js/plugins.js
+++ js/plugins.js
@@ -22,3 +22,19 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+var UTILS = (function () {
+
+    return {
+        /**
+         * Check if a given value is a plain Object
+         *
+         * @param  {*}       o Any value to be checked
+         * @return {Boolean}   true if it's an Object
+         */
+        isObject: function (o) {
+            var toString = Object.prototype.toString;
+            return (toString.call(o) === toString.call({}));
+        }
+    
+    };
+}());
